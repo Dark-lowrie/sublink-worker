4176796aa9be5332bc2594e3c082c20c58eb240a
Dark-lowrie <92874296+Dark-lowrie@users.noreply.github.com>
2025-12-17 17:25:41 +0800

    Update clashConfig.js

--- src/config/clashConfig.js
+++ src/config/clashConfig.js
@@ -31,8 +31,7 @@ export const CLASH_CONFIG = {
         	'https://dns.google/dns-query'
 		],
 		'proxy-server-nameserver': [
-			'https://120.53.53.53/dns-query',
-			'https://223.5.5.5/dns-query'
+			'https://dns.google/dns-query'
 		],
 		'nameserver-policy': {
 			'geosite:cn,private': [
